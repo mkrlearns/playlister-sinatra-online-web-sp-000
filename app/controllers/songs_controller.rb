22e2226ae40dc588f9abbdff0a784b4a5c67107a
ryan m <contact@ryanmeek.com>
2020-03-02 03:57:18 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,11 +2,6 @@ require 'rack-flash'
 class SongsController < ApplicationController
   use Rack::Flash
 
-  get('/songs') {@songs=Song.all; erb :'/songs/index'}
-  get('/songs/new') {erb :'/songs/new'}
-  get('/songs/:slug') {@song=Song.find_by_slug(params[:slug]); erb :'songs/show'}
-  get('/songs/:slug/edit') {@song=Song.find_by_slug(params[:slug]); erb :'songs/edit'}
-
   def song_fill(song, update, string)
     song.update(params[:song]) if update
     song.artist = Artist.find_or_create_by(name: params[:artist][:name])
@@ -16,6 +11,10 @@ class SongsController < ApplicationController
     redirect("/songs/#{song.slug}")
   end
 
+  get('/songs') {@songs=Song.all; erb :'/songs/index'}
+  get('/songs/new') {erb :'/songs/new'}
+  get('/songs/:slug') {@song=Song.find_by_slug(params[:slug]); erb :'songs/show'}
+  get('/songs/:slug/edit') {@song=Song.find_by_slug(params[:slug]); erb :'songs/edit'}
   post('/songs') {song_fill(Song.create(params[:song]), false, "created")}
   patch('/songs/:slug') {song_fill(Song.find_by_slug(params[:slug]), true, "updated")}
 
